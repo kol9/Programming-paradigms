1e8d83f430b1fd860bb55d77423eec2cb19a45ea
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2019-03-19 11:40:54 +0300

    Added atan/atan2 modification

--- HW5/src/objectExpression.js
+++ HW5/src/objectExpression.js
@@ -3,11 +3,17 @@ function Variable(varName) {
 }
 
 Variable.prototype.toString = function () {
-    return this.varName + " ";
+    return this.varName;
 };
 
-Variable.prototype.evaluate = function (x) {
-    return x;
+Variable.prototype.evaluate = function (x, y, z) {
+    if (this.varName === "x") {
+        return x;
+    } else if (this.varName === "y") {
+        return y;
+    } else if (this.varName === "z") {
+        return z;
+    }
 };
 
 function Const(value) {
@@ -15,36 +21,27 @@ function Const(value) {
 }
 
 Const.prototype.toString = function () {
-    return this.value.toString() + " ";
+    return this.value.toString();
 };
 
-Const.prototype.evaluate = function (x) {
+Const.prototype.evaluate = function (x, y, z) {
     return this.value;
 };
 
-/////////////////////////////////////////////////// perfect
 
 function AbstractUnaryOperation(first) {
     this.first = first;
 }
 
 AbstractUnaryOperation.prototype.toString = function () {
-    return this.first.toString() + this.operationName + " ";
+    return this.first.toString() + " " + this.operationName;
 };
 
-AbstractUnaryOperation.prototype.evaluate = function (x) {
-    return this.evalFunction(this.first.evaluate(x));
+AbstractUnaryOperation.prototype.evaluate = function (x, y, z) {
+    return this.evalFunction(this.first.evaluate(x, y, z));
 };
 
 
-function Negate(first) {
-
-    AbstractUnaryOperation.apply(this, arguments);
-    this.evalFunction = ((x) => (-x));
-    this.operationName = "-";
-}
-
-Negate.prototype = Object.create(AbstractUnaryOperation.prototype);
 
 
 function AbstractBinaryOperation(first, second) {
@@ -53,11 +50,27 @@ function AbstractBinaryOperation(first, second) {
 }
 
 AbstractBinaryOperation.prototype.toString = function () {
-    return this.first.toString() + this.second.toString() + this.operationName + " ";
+    return this.first.toString() + " " + this.second.toString() + " " + this.operationName;
 };
-AbstractBinaryOperation.prototype.evaluate = function (x) {
-    return this.evalFunction(this.first.evaluate(x), this.second.evaluate(x));
+AbstractBinaryOperation.prototype.evaluate = function (x, y, z) {
+    return this.evalFunction(this.first.evaluate(x, y, z), this.second.evaluate(x, y, z));
 };
+
+function Negate(first) {
+    AbstractUnaryOperation.apply(this, arguments);
+    this.evalFunction = ((x) => (-x));
+    this.operationName = "negate";
+}
+
+
+function ArcTan(first) {
+    AbstractUnaryOperation.apply(this, arguments);
+    this.evalFunction = ((x) => Math.atan(x));
+    this.operationName = "atan";
+}
+
+Negate.prototype = Object.create(AbstractUnaryOperation.prototype);
+ArcTan.prototype = Object.create(AbstractUnaryOperation.prototype);
 
 function Add(first, second) {
     AbstractBinaryOperation.apply(this, arguments);
@@ -82,21 +95,16 @@ function Divide(first, second) {
     this.evalFunction = ((x, y) => (x / y));
     this.operationName = "/";
 }
+function ArcTan2(first, second) {
+    AbstractBinaryOperation.apply(this, arguments);
+    this.evalFunction = ((y, x) => Math.atan2(y,x));
+    this.operationName = "atan2";
+}
 
 Negate.prototype = Object.create(AbstractUnaryOperation.prototype);
 Add.prototype = Object.create(AbstractBinaryOperation.prototype);
 Subtract.prototype = Object.create(AbstractBinaryOperation.prototype);
 Multiply.prototype = Object.create(AbstractBinaryOperation.prototype);
 Divide.prototype = Object.create(AbstractBinaryOperation.prototype);
+ArcTan2.prototype = Object.create(AbstractBinaryOperation.prototype);
 
-
-// let expr = new Negate(new Variable('x'));
-// let expr = new Add(new Variable('x'), new Add(new Variable('x'), new Variable('x')));
-// let expr = new Subtract(
-//     new Multiply(
-//         new Const(2),
-//         new Variable("x")
-//     ),
-//     new Const(3)
-// );
-// print(expr.toString());
