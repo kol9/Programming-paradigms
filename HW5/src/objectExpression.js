2b11aa6cfb2edc59ab7930ed76319f2c5540921e
Nikolay Yarlychenko <yarlychenkonikolay@gmail.com>
2019-03-26 15:13:44 +0300

    Added hard modification

--- HW5/src/objectExpression.js
+++ HW5/src/objectExpression.js
@@ -1,9 +1,19 @@
+const ONE = new Const(1);
+const ZERO = new Const(0);
+const isNumber = function (x) {
+    return (x[0] <= '9' && x[0] >= '0') || (x[0] === '-' && x.length !== 1);
+};
+
 function Variable(varName) {
     this.varName = varName;
 }
 
 Variable.prototype.toString = function () {
     return this.varName;
+};
+
+Variable.prototype.diff = function (diffVar) {
+    return this.varName === diffVar ? ONE : ZERO;
 };
 
 Variable.prototype.evaluate = function (x, y, z) {
@@ -24,145 +34,193 @@ Const.prototype.toString = function () {
     return this.value.toString();
 };
 
+Const.prototype.diff = function (diffVar) {
+    return ZERO;
+};
 Const.prototype.evaluate = function (x, y, z) {
     return this.value;
 };
 
 
-function AbstractUnaryOperation(first) {
-    this.first = first;
+function toArray(args) {
+    let arr = [];
+    arr.push.apply(arr, args);
+    return arr;
 }
 
-AbstractUnaryOperation.prototype.toString = function () {
-    return this.first.toString() + " " + this.operationName;
-};
-
-AbstractUnaryOperation.prototype.evaluate = function (x, y, z) {
-    return this.evalFunction(this.first.evaluate(x, y, z));
-};
-
-
-function AbstractBinaryOperation(first, second) {
-    this.first = first;
-    this.second = second;
+function AbstractOperation() {
+    this.args = toArray(arguments);
 }
 
-AbstractBinaryOperation.prototype.toString = function () {
-    return this.first.toString() + " " + this.second.toString() + " " + this.operationName;
-};
-AbstractBinaryOperation.prototype.evaluate = function (x, y, z) {
-    return this.evalFunction(this.first.evaluate(x, y, z), this.second.evaluate(x, y, z));
+AbstractOperation.prototype.toString = function () {
+    return this.args.map((expression) => {
+        return (expression.toString());
+}).join(" ") + " " + this.operationName;
 };
 
-function Negate(first) {
-    AbstractUnaryOperation.apply(this, arguments);
+AbstractOperation.prototype.diff = function (diffVar) {
+    return this.diffFunction(this, diffVar);
+};
+
+AbstractOperation.prototype.evaluate = function (x, y, z) {
+    return this.evalFunction.apply(null, this.args.map((expression) => {
+        return expression.evaluate.apply(expression, arguments);
+}));
+};
+
+
+function Negate() {
+    AbstractOperation.apply(this, arguments);
     this.evalFunction = ((x) => (-x));
+    this.diffFunction = (operand, diffVar) =>
+    new Negate(
+        operand.args[0].diff(diffVar)
+    );
     this.operationName = "negate";
 }
 
-
-function ArcTan(first) {
-    AbstractUnaryOperation.apply(this, arguments);
-    this.evalFunction = ((x) => Math.atan(x));
-    this.operationName = "atan";
-}
-
-
-Negate.prototype = Object.create(AbstractUnaryOperation.prototype);
-ArcTan.prototype = Object.create(AbstractUnaryOperation.prototype);
-
-function Add(first, second) {
-    AbstractBinaryOperation.apply(this, arguments);
+function Add() {
+    AbstractOperation.apply(this, arguments);
     this.evalFunction = ((x, y) => (x + y));
+    this.diffFunction = (operands, diffVar) =>
+    new Add(
+        operands.args[0].diff(diffVar),
+        operands.args[1].diff(diffVar)
+    );
     this.operationName = "+";
 }
 
-function Subtract(first, second) {
-    AbstractBinaryOperation.apply(this, arguments);
+function Subtract() {
+    AbstractOperation.apply(this, arguments);
     this.evalFunction = ((x, y) => (x - y));
+    this.diffFunction = (operands, diffVar) =>
+    new Subtract(
+        operands.args[0].diff(diffVar),
+        operands.args[1].diff(diffVar)
+    );
     this.operationName = "-";
 }
 
-function Multiply(first, second) {
-    AbstractBinaryOperation.apply(this, arguments);
+function Multiply() {
+    AbstractOperation.apply(this, arguments);
     this.evalFunction = ((x, y) => (x * y));
+    this.diffFunction = (operands, diffVar) =>
+    new Add(
+        new Multiply(operands.args[0].diff(diffVar), operands.args[1]),
+        new Multiply(operands.args[0], operands.args[1].diff(diffVar))
+    );
     this.operationName = "*";
 }
 
-function Divide(first, second) {
-    AbstractBinaryOperation.apply(this, arguments);
+function Divide() {
+    AbstractOperation.apply(this, arguments);
     this.evalFunction = ((x, y) => (x / y));
+    this.diffFunction = (operands, diffVar) =>
+    new Divide(
+        new Subtract(
+            new Multiply(operands.args[0].diff(diffVar), operands.args[1]),
+            new Multiply(operands.args[0], operands.args[1].diff(diffVar))),
+        new Multiply(operands.args[1], operands.args[1])
+    );
     this.operationName = "/";
 }
 
-function ArcTan2(first, second) {
-    AbstractBinaryOperation.apply(this, arguments);
-    this.evalFunction = ((y, x) => Math.atan2(y, x));
-    this.operationName = "atan2";
-}
-
-Negate.prototype = Object.create(AbstractUnaryOperation.prototype);
-Add.prototype = Object.create(AbstractBinaryOperation.prototype);
-Subtract.prototype = Object.create(AbstractBinaryOperation.prototype);
-Multiply.prototype = Object.create(AbstractBinaryOperation.prototype);
-Divide.prototype = Object.create(AbstractBinaryOperation.prototype);
-ArcTan2.prototype = Object.create(AbstractBinaryOperation.prototype);
-
-
-function AbstractTreeOperation(a, b, c) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-}
-
-function min2(x, y) {
-    if (x < y) return x;
-    else return y;
-}
-
-
 function Min3() {
-    AbstractTreeOperation.apply(this, arguments);
-    this.evalFunction = (x, y, z) => (min2(x, min2(y, z)));
+    AbstractOperation.apply(this, arguments);
+    this.evalFunction = Math.min;
+    this.diffFunction = (operands, diffVar) => new Min3(
+        operands.args[0].diff(diffVar),
+        operands.args[1].diff(diffVar),
+        operands.args[2].diff(diffVar),
+    );
     this.operationName = "min3";
 }
 
-AbstractTreeOperation.prototype.toString = function () {
-    return this.a.toString() + " " + this.b.toString() + " " + this.c.toString() + " " + this.operationName;
-};
-AbstractTreeOperation.prototype.evaluate = function (x, y, z) {
-    return this.evalFunction(this.a.evaluate(x, y, z), this.b.evaluate(x, y, z), this.c.evaluate(x, y, z));
-};
-
-Min3.prototype = Object.create(AbstractTreeOperation.prototype);
-
-
-function max2(x, y) {
-    if (x > y) return x;
-    else return y;
-}
-
-function AbstractFiveOperation(a, b, c, d, e) {
-    this.a = a;
-    this.b = b;
-    this.c = c;
-    this.d = d;
-    this.e = e;
-}
-
-
-AbstractFiveOperation.prototype.toString = function () {
-    return this.a.toString() + " " + this.b.toString() + " " + this.c.toString() + " " + this.d.toString() + " " + this.e.toString() + " " + this.operationName;
-};
-AbstractFiveOperation.prototype.evaluate = function (x, y, z) {
-    return this.evalFunction(this.a.evaluate(x, y, z), this.b.evaluate(x, y, z), this.c.evaluate(x, y, z), this.d.evaluate(x, y, z), this.e.evaluate(x, y, z));
-};
-
-
 function Max5() {
-    AbstractFiveOperation.apply(this, arguments);
-    this.evalFunction = (x, y, z, n, m) => (max2(x, max2(y, max2(z, max2(n, m)))));
+    AbstractOperation.apply(this, arguments);
+    this.evalFunction = Math.max;
+    this.diffFunction = (operands, diffVar) => new Max5(
+        operands.args[0].diff(diffVar),
+        operands.args[1].diff(diffVar),
+        operands.args[2].diff(diffVar),
+        operands.args[3].diff(diffVar),
+        operands.args[4].diff(diffVar));
     this.operationName = "max5";
 }
 
-Max5.prototype = Object.create(AbstractFiveOperation.prototype);
+function Sinh() {
+    AbstractOperation.apply(this, arguments);
+    this.evalFunction = Math.sinh;
+    this.diffFunction = (operand, diffVar) => {
+        return new Multiply(
+            new Cosh(operand.args[0]),
+            operand.args[0].diff(diffVar)
+        );
+    };
+    this.operationName = "sinh"
+}
+
+function Cosh() {
+    AbstractOperation.apply(this, arguments);
+    this.evalFunction = Math.cosh;
+    this.diffFunction = (operand, diffVar) =>
+    new Multiply(
+        new Sinh(operand.args[0]),
+        operand.args[0].diff(diffVar)
+    );
+    this.operationName = "cosh"
+}
+
+Negate.prototype = Object.create(AbstractOperation.prototype);
+Add.prototype = Object.create(AbstractOperation.prototype);
+Subtract.prototype = Object.create(AbstractOperation.prototype);
+Multiply.prototype = Object.create(AbstractOperation.prototype);
+Divide.prototype = Object.create(AbstractOperation.prototype);
+Min3.prototype = Object.create(AbstractOperation.prototype);
+Max5.prototype = Object.create(AbstractOperation.prototype);
+Sinh.prototype = Object.create(AbstractOperation.prototype);
+Cosh.prototype = Object.create(AbstractOperation.prototype);
+
+
+let parse = function (expression) {
+    let stack = [];
+    let variables = {
+        "x": Variable,
+        "y": Variable,
+        "z": Variable
+    };
+    let operations = {
+        "negate": [Negate, 1],
+        '+': [Add, 2],
+        '-': [Subtract, 2],
+        '*': [Multiply, 2],
+        '/': [Divide, 2],
+        "min3": [Min3, 3],
+        "max5": [Max5, 5],
+        "sinh": [Sinh, 1],
+        "cosh": [Cosh, 1],
+    };
+
+    let tokens = expression.split(' ').filter(token => token.length > 0);
+    tokens.map(function (token) {
+            if (token in operations) {
+                let operation = operations[token][0];
+                let args = [];
+                stack.slice(stack.length - operations[token][1], stack.length).forEach(() => {
+                    args.push(stack.pop());
+            });
+                let expr = new operation;
+                args.reverse();
+                operation.apply(expr, args);
+                stack.push(expr);
+            } else if (isNumber(token)) {
+                stack.push(new Const(parseFloat(token)));
+            } else if (token in variables) {
+                stack.push(new Variable(token));
+            }
+        }
+    );
+
+    return stack.pop();
+};
+
